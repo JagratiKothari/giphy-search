4923aab1a2c73ceb931f6df758913ce2a7122f18
Jagrati <sanjaysamal@Apples-MacBook-Pro.local>
2022-06-01 14:24:20 +0530

    api call file

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 const GIF_API_KEY = process.env.REACT_APP_GIPHY_KEY;
 
-export const getGifs = (searchInput, offset) => {
+export const getGifs = (searchInput, offset = 100) => {
     return  axios.get(`https://api.giphy.com/v1/gifs/search?q=${searchInput}&offset=${offset}&api_key=${GIF_API_KEY}`)
 }
